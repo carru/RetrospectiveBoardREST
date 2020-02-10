292bfcb132634a8926a57c8fb348610aebebbe2f
carru <carru400@gmail.com>
2020-02-09 19:11:59 -0800

    Reuse same server for socket

--- server/server.js
+++ server/server.js
@@ -4,12 +4,10 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
-app.start = function() {
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -17,26 +15,21 @@ app.start = function() {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
-
-    // Start socket server
-    http.listen(3001, function() {
-      console.log('Socket server listening on *:3001');
-    });
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
-
-io.on('connection', socket => {
-  socket.on('dataChanged', () => {
-    socket.broadcast.emit('dataChanged');
-  });
+  if (require.main === module) {
+    app.io = require('socket.io')(app.start());
+    app.io.on('connection', socket => {
+      socket.on('dataChanged', () => {
+        socket.broadcast.emit('dataChanged');
+      });
+    });
+  }
 });
